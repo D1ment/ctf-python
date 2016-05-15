3fb5c3be100da36dac49a0338cccd546317294d1
D1ment <d1ment@sandbox-ctf.com>
2016-05-15 21:22:24 +0300

    d1ment
    Декодеры

---
+++ crypto/crypto.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+import socket
+
+#######################################################
+#Morze decod
+def morze_dec(text_dec):
+	url = 'https://gc.de/async.php'
+	payload = {'decoded': '', 'key': '', 'encoded': text_dec, 'method': 'morse', 'decode': '1', 'encode': ''}
+	r = requests.post(url, data=payload)
+	js_new = json.loads(r.text)
+	return js_new['decoded']
+
+#######################################################
+#Podstanovka decod
+
+def podst_dec(text_dec):
+	
+
+
+
+sock = socket.socket()
+sock.connect(('146.148.102.236', 24069))
+data = sock.recv(200240)
+print data
+print '====================='
+while 1:
+	sock.send('a b c d e f g h i\n')
+	data = sock.recv(200240)
+	print data
+	text = data[data.find('What is '):data.find('  decrypted?')]
+	text = text[8:]
+	text = morze_dec(text)
+	sock.send(text + '\n')
+	data = sock.recv(200240)
+	print data
