4fac0b10a225a67f0c5bdb2aebcf2f5a5ef5b8bd
D1ment <romanovda24@gmail.com>
2016-05-30 00:01:36 +0400

    Update vvodotvetov.py

--- nc program/vvodotvetov.py
+++ nc program/vvodotvetov.py
@@ -5,7 +5,8 @@ sock = socket.socket()
 sock.connect(('p.tjctf.org', 8006))
 data = sock.recv(200240)
 
-direction1 = ['north', 'west', 'examine journal2', 'east', 'east', 'east', 'examine paintedeye', 'down', 'examine book', 'up', 'west', 'west', 'south', 'south', 'examine paper' ]
+direction1 = ['north', 'west', 'examine journal2', 'east', 'east', 'east', 'examine paintedeye', 'down', 'examine book', 'up', 
+'west', 'west', 'south', 'south', 'examine paper' ]
 
 for elem in direction:
 	sock.send(elem + '\n')
